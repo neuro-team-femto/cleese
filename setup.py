2a22159e473e02069eb185c831a9fe13c1b2607a
Seung-Goo Kim <dr.seunggoo.kim@gmail.com>
2022-07-05 13:29:13 +0200

    put back tutorial files to the original locations, and updated  to install package=['cleese']

--- setup.py
+++ setup.py
@@ -2,4 +2,5 @@ from setuptools import setup
 
 setup(name='cleese',
       version='0.1.0',
-      install_requires=['numpy', 'scipy'])
+      packages=['cleese'],
+      install_requires=['numpy', 'scipy', 'ipython'])
