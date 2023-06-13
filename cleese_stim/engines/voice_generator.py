de95cefa1169c1dfd458d56ba309d56a881573ed
chocobearz <ptuttosi@gmail.com>
2023-06-13 15:21:32 +0200

    addition of randomization for pitch and speed

--- cleese_stim/engines/voice_generator.py
+++ cleese_stim/engines/voice_generator.py
@@ -3,6 +3,8 @@
 
 import azure.cognitiveservices.speech as speechsdk
 from cleese_stim.engines.engine import Engine
+import numpy as np
+import sys
 
 class VoiceGenerator(Engine):
 
@@ -32,15 +34,29 @@ class VoiceGenerator(Engine):
       </speak>
     '''
 
+    def createRandomValues(feature_config, num_words):
+      # random BPF values
+      # center is set as per azure reccomended values
+      values = np.random.normal(0, feature_config['std'], num_words)
+
+      # eliminate far samples (trunc factor)
+      # and replace them by new random values
+      for i, value in enumerate(values):
+          while np.abs(value) > (feature_config['std'] * feature_config['trunc']):
+              values[i] = np.random.normal(0, feature_config['std'], 1)
+
+      return(values)
+
     text = open(text_data, "r").read()
     text_list = text.split(' ')
-    pitch_list = []
-    rate_list = []
     ssml = []
+    random_feature_values = {}
 
-    ###########Randomization of the rate and pitch here ##################
-    ##                                                                  ##
-    ######################################################################
+    for feature in features:
+      if feature == 'pitch':
+        random_feature_values['pitch'] = createRandomValues(config['pitch'] ,len(text_list))
+      if feature == 'speed':
+        random_feature_values['speed'] = createRandomValues(config['speed'] ,len(text_list))
 
     for i,word in enumerate(text_list) :
       template = base_template
@@ -49,33 +65,39 @@ class VoiceGenerator(Engine):
       #####################################################
       for feature in features :
         if feature == 'pitch' :
-          template = template.replace("FEATURE", f'pitch="{pitch_list[i]}" FEATURE')
+          if random_feature_values["pitch"][i] > 0 :
+            template = template.replace("FEATURE", f'pitch="+{round(random_feature_values["pitch"][i])}%" FEATURE')
+          else:
+            template = template.replace("FEATURE", f'pitch="{round(random_feature_values["pitch"][i])}%" FEATURE')
         elif feature == 'speed' :
-          template = template.replace("FEATURE", f'rate="{rate_list[i]}" FEATURE')
+          if random_feature_values["speed"][i] > 0 :
+            template = template.replace("FEATURE", f'rate="+{round(random_feature_values["speed"][i])}%" FEATURE')
+          else:
+            template = template.replace("FEATURE", f'rate="{round(random_feature_values["speed"][i])}%" FEATURE')
         else :
           print("incorrect feature input : please supply any combination of 'speed', 'pitch'")
-          exit(1)
+          sys.exit(1)
 
       # remove the FEATURE tag for templating
       template = template.replace(" FEATURE", '')
       template = template.replace("WORD", word)
       ssml.append(template)
 
-    ssml_template = ssml_template.replace("LANGUAGE", language)
-    ssml_template = ssml_template.replace("VOICE", voice)
-    ssml_template = ssml_template.replace("TEXT", '\n'.join(ssml))
+    ssml_input = ssml_template.replace("LANGUAGE", language)
+    ssml_input = ssml_input.replace("VOICE", voice)
+    ssml_input = ssml_input.replace("TEXT", '\n'.join(ssml))
 
     #Run TTS synthesis in Microsoft Azure, requires account with key
     try:
       speech_config = speechsdk.SpeechConfig(subscription=key, region=region)
-      audio_config = speechsdk.audio.AudioOutputConfig(filename = output_file)
+      audio_config = speechsdk.audio.AudioOutputConfig(filename=output_file)
 
-      synthesizer = speechsdk.SpeechSynthesizer(speech_config, audio_config)
+      speech_synthesizer = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-      result = synthesizer.speak_ssml_async(ssml).get()
+      result = speech_synthesizer.speak_ssml_async(ssml_input).get()
 
       if result.reason == speechsdk.ResultReason.SynthesizingAudioCompleted:
-        print("Speech synthesized for text [{}]".format(text_data))
+          print("Speech synthesized for text [{}]".format(text))
       elif result.reason == speechsdk.ResultReason.Canceled:
           cancellation_details = result.cancellation_details
           print("Speech synthesis canceled: {}".format(cancellation_details.reason))
@@ -88,3 +110,5 @@ class VoiceGenerator(Engine):
       stream.save_to_wav_file(output_file)
     except Exception as e:
       print(e)
+
+    return(stream, random_feature_values)
