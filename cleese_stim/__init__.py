92b81cebe95e54ad5db95d09d8af6a75a42ff689
JJ Aucouturier <aucouturier@gmail.com>
2024-05-01 09:46:32 +0200

    removed tutorial

--- cleese_stim/__init__.py
+++ cleese_stim/__init__.py
@@ -1,4 +1,4 @@
 from .cleese import process_file, process_data, generate_stimuli, load_config
 from . import engines
 
-__version__ = "2.0.0"
+__version__ = "2.2.0"
--- cleese_stim/engines/__init__.py
+++ cleese_stim/engines/__init__.py
@@ -1,2 +1,2 @@
 from .phase_vocoder.phase_vocoder import PhaseVocoder
-from .mediapipe import Mediapipe
+from .face_warp.face_warp import FaceWarp
